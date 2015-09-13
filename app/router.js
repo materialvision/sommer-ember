86f0839dbe95d02ebeca9c757aedc2f6dc480753
Espen Sommer Eide <espensommer@gmail.com>
2015-09-13 22:06:05 +0200

    fix pure

--- app/router.js
+++ app/router.js
@@ -6,7 +6,11 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('pages');
+  this.route('news', { path: '/news' });
+  this.route('works', { path: '/works' });
+  this.route('bio', { path: '/bio' });
+  this.route('rss', { path: '/rss' });
+  this.resource('pages', { path: '/pages/:page_id' });
 });
 
 export default Router;
